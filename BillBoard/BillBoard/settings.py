892035e2258d72a1e2dc1c5357652cbdf469bc15
Gaidysheff <gaidysheff@mail.ru>
2022-09-12 22:42:53 +0500

    CustomUser created

--- BillBoard/BillBoard/settings.py
+++ BillBoard/BillBoard/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'customuser',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'customuser.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
