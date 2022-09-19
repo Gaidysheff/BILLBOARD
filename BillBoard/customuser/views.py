c769380c060a9a324233ef2e58e3590a196817bc
Gaidysheff <gaidysheff@mail.ru>
2022-09-19 10:46:15 +0500

    { registered_user } in email

--- BillBoard/customuser/views.py
+++ BillBoard/customuser/views.py
@@ -3,6 +3,10 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+from .models import CustomUser
 
 
 class SignUp(generic.CreateView):
@@ -10,19 +14,22 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+    users = CustomUser.objects.all()
+    registered_user = users[len(users)-1].email
+
     """ ВАРИАНТ-1. В этом варианте все адреса получателей будут видны всем получателям одновременно"""
 
     send_mail(
-        subject='OPTION-1',
-        message='ПРОВЕРКА СОХРАНЯЕМОСТИ В ФАЙЛ. SUBJECT - НЕЧИТАЕМЫЙ',
+        subject='SENDING OPTION-1',
+        message=f'Вы зарегистрировались на MMORPG сайтe. { registered_user }',
         from_email='gaidysheff@yandex.ru',
         recipient_list=['gaidysheff@mail.ru', 'gaidysheff@gmail.com', ]
     )
 
     """ ВАРИАНТ-2. В этом варианте адресат будет видеть только свой адрес в графе 'кому'."""
 
-    Subject = 'OPTION-2'
-    Message = 'ПРОВЕРКА СОХРАНЯЕМОСТИ В ФАЙЛ. SUBJECT - НЕЧИТАЕМЫЙ'
+    Subject = 'SENDING OPTION-2'
+    Message = f'Добро пожаловать на MMORPG сайт. Вы зарегистрировались как: { registered_user } '
 
     datatuple = (
         (Subject, Message, 'gaidysheff@yandex.ru',
