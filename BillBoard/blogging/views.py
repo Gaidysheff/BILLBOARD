6592a57bdd3159758e2f971cc4f60ea0c6eaa2a4
Gaidysheff <gaidysheff@mail.ru>
2022-09-29 17:28:22 +0500

    Authentication for Update & Delete

--- BillBoard/blogging/views.py
+++ BillBoard/blogging/views.py
@@ -146,12 +146,57 @@ class UpdateBlog(LoginRequiredMixin, DataMixin, UpdateView):
     # login_url = reverse_lazy('home')
     # raise_exception = True
 
+# ------------------- Рассмотреть блок ниже ------------------------
+
+    # def form_valid(self, form):
+    #     self.object = form.save(commit=False)
+    #     self.object.user = self.request.user
+    #     self.object.save()
+    #     return super().form_valid(form)
+
+# --------------------------------------------------------------
+
 
 class DeleteBlog(LoginRequiredMixin, DataMixin, DeleteView):
     model = Post
     template_name = 'blogging/deleteblog.html'
     success_url = reverse_lazy('home')
 
+# ------------------- Рассмотреть блок ниже ------------------------
+# class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+#     model = models.Post
+#     select_related = ("user", "group")
+#     success_url = reverse_lazy("posts:all")
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         return queryset.filter(user_id=self.request.user.id)
+
+#     def delete(self, *args, **kwargs):
+#         messages.success(self.request, "Post Deleted")
+#         return super().delete(*args, **kwargs)
+
+# -------------------------------------------------------------------------------
+
+
+class UserPosts(LoginRequiredMixin, DataMixin, ListView):
+    model = Post
+    template_name = "blogging/user_post_list.html"
+
+    def get_queryset(self):
+        try:
+            self.post_user = User.objects.prefetch_related("posts").get(
+                username__iexact=self.kwargs.get("username")
+            )
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_user"] = self.post_user
+        return context
 
 # def add_comment_to_post(request, post_slug):
 #     if request.method == "POST":
