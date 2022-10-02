7862ac226ffaa4c6d2c23608b0134abf88f08623
Gaidysheff <gaidysheff@mail.ru>
2022-10-02 16:08:54 +0500

    just check

--- BillBoard/blogging/signals.py
+++ BillBoard/blogging/signals.py
@@ -4,13 +4,13 @@ from django.core.mail import mail_managers
 from .models import Post
 
 
-@receiver(post_save, sender=Post)
-def notify_post(sender, instance, created, **kwargs):
-    if created:
-        subject = f'Опубликован новый пост "{instance.post.title}" {instance.date.strftime("%d %m %Y")}'
-    else:
-        subject = f'Пост "{instance.post.title}" изменён {instance.date.strftime("%d %m %Y")}'
-    mail_managers(
-        subject=subject,
-        message=instance.message,
-    )
+# @receiver(post_save, sender=Post)
+# def notify_post(sender, instance, created, **kwargs):
+#     if created:
+#         subject = f'Опубликован новый пост "{instance.post.title}" {instance.date.strftime("%d %m %Y")}'
+#     else:
+#         subject = f'Пост "{instance.post.title}" изменён {instance.date.strftime("%d %m %Y")}'
+#     mail_managers(
+#         subject=subject,
+#         message=instance.message,
+#     )
